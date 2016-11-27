32bfae73ee1e54fa1171e4cb05ffa8ba7ee1de17
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-26 22:38:41 -0700

    Reformat

--- menuFunctions.js
+++ menuFunctions.js
@@ -1,33 +1,33 @@
 const {dialog} = require('electron');
 
 const openDialogOptions = {
-  filters: [
-    {name: "eBooks", extensions: ['epub']}
-  ],
-  properties: ['openFile']
+    filters: [
+        {name: "eBooks", extensions: ['epub']}
+    ],
+    properties: ['openFile']
 };
 
 module.exports = {
-  open: function(menuItem, browserWindow) {
-    dialog.showOpenDialog(browserWindow, openDialogOptions, (bookPaths) => {
-      if (bookPaths) {
-        browserWindow.webContents.send('loadBook', bookPaths[0]);
-      }
-    });
-  },
-  nextPage: function(menuItem, browserWindow) {
-    browserWindow.webContents.send('nextPage');
-  },
-  previousPage: function(menuItem, browserWindow) {
-    browserWindow.webContents.send('prevPage');
-  },
-  increaseFontSize: function(menuItem, browserWindow) {
-    browserWindow.webContents.send('increaseFont');
-  },
-  decreaseFontSize: function(menuItem, browserWindow) {
-    browserWindow.webContents.send('decreaseFont');
-  },
-  restoreDefaultFontSize: function(menuItem, browserWindow) {
-    browserWindow.webContents.send('restoreFont');
-  }
+    open: function(menuItem, browserWindow) {
+        dialog.showOpenDialog(browserWindow, openDialogOptions, (bookPaths) => {
+            if (bookPaths) {
+                browserWindow.webContents.send('loadBook', bookPaths[0]);
+            }
+        });
+    },
+    nextPage: function(menuItem, browserWindow) {
+        browserWindow.webContents.send('nextPage');
+    },
+    previousPage: function(menuItem, browserWindow) {
+        browserWindow.webContents.send('prevPage');
+    },
+    increaseFontSize: function(menuItem, browserWindow) {
+        browserWindow.webContents.send('increaseFont');
+    },
+    decreaseFontSize: function(menuItem, browserWindow) {
+        browserWindow.webContents.send('decreaseFont');
+    },
+    restoreDefaultFontSize: function(menuItem, browserWindow) {
+        browserWindow.webContents.send('restoreFont');
+    }
 }
