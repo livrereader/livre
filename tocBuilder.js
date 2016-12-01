83fafb692f2bb13d7623e5bb3ddb8b848c75d923
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-12-01 14:50:19 -0700

    Clear forward buffer when location changes by any means other than 'back'. Do not add duplicate CFIs to either buffer

--- tocBuilder.js
+++ tocBuilder.js
@@ -1,4 +1,4 @@
-const buildTocListItem = function(tocItem, Book, backBuffer) {
+const buildTocListItem = function(tocItem, Book, backBuffer, forwardBuffer) {
     const $listItem = document.createElement('li');
 
     const $stylingSpan = document.createElement('span');
@@ -10,6 +10,7 @@ const buildTocListItem = function(tocItem, Book, backBuffer) {
     $listItem.onclick = function(event) {
         event.stopPropagation();
         backBuffer.push(Book.renderer.currentLocationCfi);
+        forwardBuffer = [];
         Book.goto(tocItem.href);
     };
    
@@ -17,7 +18,7 @@ const buildTocListItem = function(tocItem, Book, backBuffer) {
         const $subList = document.createElement('ul');
         for (let i in tocItem.subitems) {
             let subItem = tocItem.subitems[i];
-            let $subItemList = buildTocListItem(subItem, Book, backBuffer);
+            let $subItemList = buildTocListItem(subItem, Book, backBuffer, forwardBuffer);
             $subList.appendChild($subItemList);
         }
         $listItem.appendChild($subList);
@@ -26,11 +27,11 @@ const buildTocListItem = function(tocItem, Book, backBuffer) {
     return $listItem;
 }
 
-module.exports = function(toc, Book, backBuffer) {
+module.exports = function(toc, Book, backBuffer, forwardBuffer) {
     const $tocList = document.createElement('ul');
     for (let i in toc) {
         let tocItem = toc[i];
-        let $listItem = buildTocListItem(tocItem, Book, backBuffer);
+        let $listItem = buildTocListItem(tocItem, Book, backBuffer, forwardBuffer);
         $tocList.appendChild($listItem);
     }
     return $tocList;
