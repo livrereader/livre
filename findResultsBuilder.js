ad5d15da5eaeed6adedc19e94967978d1c1a85a4
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-07-06 00:23:58 -0400

    Use new helper functions

--- findResultsBuilder.js
+++ findResultsBuilder.js
@@ -1,38 +1,17 @@
-const buildResultListItem = function(result, Book, backBuffer, forwardBuffer) {
-    const $result = document.createElement("li");
-    $result.classList = "findResult";
-    const location = Book.locations.locationFromCfi(result.cfi);
-    const resultHTML = `<div>
-                            <span class="findResultExcerpt">${result.excerpt.trim()}</span>
-                            <br>
-                            <span class="findResultLocation">Location: ${location}</span>
-                        </div>`;
-    $result.innerHTML = resultHTML;
-
-    $result.onclick = function(event) {
-        event.stopPropagation();
-        backBuffer.push(Book.renderer.currentLocationCfi);
-        forwardBuffer = [];
-        Book.goto(result.cfi);
-    };
-
-    return $result;
-};
+const menuItem = require('./menuItem');
+const menuList = require('./menuList');
 
 const buildFindResults = function(results, Book, backBuffer, forwardBuffer) {
-    const $resultsList = document.createElement("ul");
-    $resultsList.id = "findResults"
-    for (let i in results) {
-        const result = results[i];
-        const $result = buildResultListItem(
-            result,
-            Book,
-            backBuffer,
-            forwardBuffer
-        );
-        $resultsList.appendChild($result);
-    }
-    return $resultsList;
+    const resultItems = results.map(result => {
+        const location = Book.locations.locationFromCfi(result.cfi);
+        return menuItem(result.excerpt.trim(), `Location: ${location}`, null, event => {
+            event.stopPropagation();
+            backBuffer.push(Book.renderer.currentLocationCfi);
+            forwardBuffer = [];
+            Book.goto(result.cfi);
+        });
+    });
+    return menuList(resultItems);
 };
 
 module.exports = buildFindResults;
