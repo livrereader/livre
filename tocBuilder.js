15f9b89503e5eec1c80a5ecdfe258156524d167d
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-07-09 15:47:26 -0400

    Properly generate TOC subitems using new helper functions

--- tocBuilder.js
+++ tocBuilder.js
@@ -3,57 +3,42 @@ const menuList = require('./menuList');
 
 const buildTocListItem = function(tocItem, Book, backBuffer, forwardBuffer) {
     const location = Book.locations.locationFromCfi(tocItem.cfi);
-    const body = tocItem.subItems && tocItem.subitems.length > 0
-          ? 'TODO'
-          : tocItem.label.trim();
-    return menuItem({
-        body: body,
-        footer: `Location: ${location}`,
-        onclick: event => {
-            event.stopPropagation();
-            backBuffer.push(Book.renderer.currentLocationCfi);
-            forwardBuffer = [];
-            Book.goto(tocItem.href);
-        }
-    });
 
-    if (tocItem.subitems && tocItem.subitems.length > 0) {
-        const $subList = document.createElement("ul");
-        for (let i in tocItem.subitems) {
-            let subItem = tocItem.subitems[i];
-            let $subItemList = buildTocListItem(
-                subItem,
-                Book,
-                backBuffer,
-                forwardBuffer
-            );
-            $subList.appendChild($subItemList);
-        }
-        $listItem.appendChild($subList);
-    }
+    const onclick = event => {
+        event.stopPropagation();
+        backBuffer.push(Book.renderer.currentLocationCfi);
+        forwardBuffer = [];
+        Book.goto(tocItem.href);
+    };
+
+    const $tocItem = menuItem({
+        body: tocItem.label.trim(),
+        footer: `Location: ${location}`,
+        onclick: onclick
+    });
+    
+    const parentItem = () => {
+        const $wrapper = document.createElement('li');
+        const $item = $tocItem;
+        const $subitem = menuList({
+            items: tocItem.subitems.map(subitem =>
+                buildTocListItem(subitem, Book, backBuffer, forwardBuffer)
+            )
+        });
+        $wrapper.appendChild($subitem);
+        $wrapper.insertBefore($item, $subitem);
+        return $wrapper;
+    };
+
+    return tocItem.subitems && tocItem.subitems.length > 0
+        ? parentItem()
+        : $tocItem;
 };
 
 module.exports = function(toc, Book, backBuffer, forwardBuffer) {
-    const tocItems = toc.map(tocItem => buildTocListItem(tocItem, Book, backBuffer, forwardBuffer));
-
     return menuList({
-        items: tocItems,
+        items: toc.map(tocItem => buildTocListItem(tocItem, Book, backBuffer, forwardBuffer)),
         id: 'table-of-contents',
         classList: [ 'toc' ]
     });
-
-    const $tocList = document.createElement("ul");
-    $tocList.id = "table-of-contents";
-    $tocList.classList = "toc";
-    for (let i in toc) {
-        let tocItem = toc[i];
-        let $listItem = buildTocListItem(
-            tocItem,
-            Book,
-            backBuffer,
-            forwardBuffer
-        );
-        $tocList.appendChild($listItem);
-    }
-    return $tocList;
 };
