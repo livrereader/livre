f0934d10fbc7ffe03a65a1c44255d9828c2cc9b0
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-07-13 18:42:17 -0400

    Bundle renderer assets with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    target: 'electron-renderer'
+};
